4ca05f333d920691d497941ba02b340d216e1fa4
Vitaliy <37402578+VitaliyPolischuk@users.noreply.github.com>
2018-12-07 20:26:57 +0200

    adaptation for gtts updation

--- processing_audio.py
+++ processing_audio.py
@@ -6,6 +6,35 @@ from pydub import AudioSegment
 from pydub.playback import play
 from gtts import gTTS
 
+import calendar
+import time
+import math
+import re
+import requests
+from gtts import gTTS
+from gtts_token.gtts_token import Token
+
+def _patch_faulty_function(self):
+    if self.token_key is not None:
+        return self.token_key
+
+    timestamp = calendar.timegm(time.gmtime())
+    hours = int(math.floor(timestamp / 3600))
+
+    results = requests.get("https://translate.google.com/")
+    tkk_expr = re.search("(tkk:*?'\d{2,}.\d{3,}')", results.text).group(1)
+    tkk = re.search("(\d{5,}.\d{6,})", tkk_expr).group(1)
+    
+    a , b = tkk.split('.')
+
+    result = str(hours) + "." + str(int(a) + int(b))
+    self.token_key = result
+    return result
+
+
+# Monkey patch faulty function.
+Token._get_token_key = _patch_faulty_function
+
 def synthesize_text(text, lang ,audio_output='output.mp3'):
     tts=gTTS(text=text, lang=lang)
     tts.save(audio_output)
